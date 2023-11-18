b15fa538b2917fefa9ceafdaf3413d72be93d263
guiireal <guii_real@hotmail.com>
2023-11-18 12:27:27 -0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Navbar />
+  <component is="Navbar" />
+  <header>
+    <TheHeader />
+  </header>
+
+  <main></main>
+</template>
+
+<script setup>
+import TheHeader from "./components/TheHeader.vue";
+</script>
+
+<style scoped>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: Verdana, Verdana, Geneva, Tahoma, sans-serif;
+}
+
+header {
+  line-height: 1.5;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1 class="title">{{ name }}</h1>
+  </div>
+</template>
+
+<script setup>
+const name = "Guilherme";
+</script>
+
+<style scoped>
+.title {
+  margin: 0 auto;
+  text-align: center;
+  color: red;
+}
+</style>
