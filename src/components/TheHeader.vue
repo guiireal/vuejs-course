c276e0b343be2c5f159762a12e70aee63efa25ca
guiireal <guii_real@hotmail.com>
2023-11-19 13:36:08 -0300

    wip

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <h1 class="title">{{ name }}</h1>
+    <span v-html="subtitle"></span>
+    <span v-text="subtitle"></span>
+    <span>{{ subtitle }}</span>
   </div>
 </template>
 
 <script setup>
 const name = "Guilherme";
+const subtitle = "<p style='color: silver'>Tutoriais Vue</p>";
 </script>
 
 <style scoped>
