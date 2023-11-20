2f92daf228c6273e53fc690a2e39c7a0533e3666
guiireal <guii_real@hotmail.com>
2023-11-19 23:06:54 -0300

    wip

--- src/pages/Team.vue
+++ src/pages/Team.vue
@@ -5,16 +5,30 @@
     <button @click="officeStore.removeOffice(office.id)">Remover cargo</button>
   </div>
   <div v-for="color in colors" :key="color">
-    {{ color }}
+    {{ color }} <button @click="changeColor(color)">Mudar cor</button>
   </div>
+  <div class="panel"></div>
 </template>
 
 <script setup>
 import { storeToRefs } from "pinia";
+import { ref } from "vue";
 import { useOfficeStore } from "../store/officeStore";
 
+const colorSelected = ref("#fff");
 const officeStore = useOfficeStore();
 const { offices, colors } = storeToRefs(officeStore);
+
+const changeColor = (color) => {
+  colorSelected.value = color;
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+.panel {
+  width: 70px;
+  height: 30px;
+  border: 1px solid;
+  background: v-bind(colorSelected);
+}
+</style>
